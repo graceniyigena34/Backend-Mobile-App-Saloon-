a13ed5685fadc948eec839d2260eb9d2351b5922
graceniyigena34 <graceniyigena34@gmail.com>
2026-01-30 16:33:12 +0200

    Backend Mobile App

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: 'customer' | 'owner';
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['customer', 'owner'],
+      default: 'customer',
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
+dotenv.config();
+
+const app = express();
+
+// Connect MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Mobile Salon Backend is running 🚀');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
